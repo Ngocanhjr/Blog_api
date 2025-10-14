56f288e5ab8d009adf91dcd8856c91d7e7067d7a
human6004 <letranhieunhanvn@gmail.com>
2025-10-14 20:15:01 +0700

    add API get,put,delete

---
+++ src/main/java/ctu/edu/blogAPI/dto/response/UserRespone.java
@@ -0,0 +1,23 @@
+package ctu.edu.blogAPI.dto.response;
+
+import java.time.LocalDate;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserRespone {
+    String id;
+    String username;
+    String password;
+    String fullname;
+    LocalDate dob;
+}
