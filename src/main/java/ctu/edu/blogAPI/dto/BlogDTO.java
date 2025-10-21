63791c9b0dd34fc134b6cdd69d3f0c709f152fc7
Lengocanh <Lengocanh3524@gmail.com>
2025-10-22 00:44:01 +0700

    rename merge/post/dev to ft/post

---
+++ src/main/java/ctu/edu/blogAPI/dto/BlogDTO.java
@@ -0,0 +1,36 @@
+package ctu.edu.blogAPI.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import org.bson.types.ObjectId;
+
+import java.time.Instant;
+import java.util.List;
+
+@Data
+@Builder
+public class BlogDTO {
+    private String id;
+
+    private String userId;
+
+    private String userName;
+
+    private String userAvatarUrl;
+
+    private String content;
+
+    private List<String> imageContentUrls;
+
+    private Long likeCount;
+
+    private Long commentCount;
+
+    private Long shareCount;
+
+    private boolean published = true;
+
+    private Instant createdAt;
+
+    private  Instant updateAt;
+}
