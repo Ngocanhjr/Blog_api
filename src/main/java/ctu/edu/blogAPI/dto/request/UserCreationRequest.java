a4d02c350898fffc1bd2f8e89a91944fd28669d9
human6004 <letranhieunhanvn@gmail.com>
2025-11-03 10:28:23 +0700

    update UserCreationRequest

--- src/main/java/ctu/edu/blogAPI/dto/request/UserCreationRequest.java
+++ src/main/java/ctu/edu/blogAPI/dto/request/UserCreationRequest.java
@@ -20,10 +20,13 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor //tạo constructor với tất cả field theo thứ tự khai báo.
 @FieldDefaults(level = AccessLevel.PRIVATE) // ko cần khai khai báo các phạm vi của biến
 public class UserCreationRequest {
-    @NotBlank(message = "Tên tài khoản không được để trống")
-    @Size(min = 3, message = "username must be at least 3 character!!!")
-    String username;
+     @NotBlank(message = "Tên tài khoản không được để trống")
+    @Size(min = 3, max = 30, message = "Username 3–30 ký tự")
+    // CHỌN 1 TRONG 2 regex dưới đây:
 
+    // Chữ + số (phổ biến, vẫn không khoảng trắng/ký tự đặc biệt):
+    @Pattern(regexp = "^[a-z0-9]+$", message = "Username chỉ gồm a-z, 0-9, không khoảng trắng")
+    String username;
 
     @NotBlank(message = "Mật khẩu không được để trống")
     @Size(min = 8, message = "password must be at least 8 character!!!")
@@ -33,11 +36,9 @@ public class UserCreationRequest {
     )
     String password;
 
-
     @NotNull(message = "Họ tên không được để trống")
     String fullname;
-    
-    
+
     @Past(message = "Ngày sinh phải nhỏ hơn ngày hiện tại")
     LocalDate dob;
 
